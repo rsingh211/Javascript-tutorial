04a00edef61c470d263773992a3080ed0f9f957a
rsingh211 <rsingh15@academic.rrc.ca>
2025-06-14 22:08:56 +0000

    Learnt Vairables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,31 @@
+/* My Notes
+ So yeah, I already knew about var and let situation but we learnt something new 
+ which was console.table and also that we can assign variable without using a keyword aswell
+ That's cool
+
+*/
+
+
+
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
