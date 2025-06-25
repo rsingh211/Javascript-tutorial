1c865cb3bdb43d37c01963f3f7f407a62b1c17ae
rsingh211 <rsingh15@academic.rrc.ca>
2025-06-25 16:40:37 -0500

    Learnt about objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,60 @@
+// const tinderUser = new Object (); //This is singleton
+
+const tinderUser = {}
+
+tinderUser.id = "123qwerty"
+tinderUser.name = "Sam"
+tinderUser.isLoggedIn = false
+
+
+const regularUser = {
+    email: "someone@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname:"Rajvir",
+            lastname: "Singh"
+        }
+    }
+
+}
+
+// console.log(regularUser.fullname?.userfullname.firstname)
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"c", 4:"d"}
+
+// const obj3 = { obj1, obj2} // returns them as seperate
+
+// const obj3 = Object.assign(obj1,obj2) // this is okay but here the values are being added into obj1
+
+// To solve that we use {} as a target and than refer our objects
+// const obj3 = Object.assign({},obj1,obj2)
+// This combines the object without changing our initial objects
+
+const obj3 = {...obj1, ...obj2} // THis is the mordern way using spread(...)
+
+// console.log(obj3)
+
+const users = [
+    {
+        id : 1,
+        alias : "Bhen"
+    },
+    {
+        id : 2,
+        alias : "Jaan"
+    },
+    {
+        id : 3,
+        alias : "Jorr"
+    }
+]
+
+console.log(users[1].alias)
+
+console.log(tinderUser)
+
+console.log(Object.keys(tinderUser)) // gives output as array
+console.log(Object.values(tinderUser)) // gives output as array
+
+console.log(Object.entries(tinderUser));
