b7c5aeec46754f5cf817ea4b62e37e439053607d
rsingh211 <rsingh15@academic.rrc.ca>
2025-07-02 08:40:56 -0500

    Basics all done

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,19 @@
+// Immediately Invoked Function Expression
+
+(function chai() {
+    console.log(`DB CONNECTED`);
+})();
+
+//Named IIEFE
+
+
+///Make sure to end this function using ; while using two IIFE
+
+( (name)=>
+    (console.log(`Hello ${name}`)
+))('Leap');
+
+
+
+// chai()
+
